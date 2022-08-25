cda5ea1a5ce02570d29feb415ce1d2efe5366b15
Matheus Vieira <matheus.msv1997@gmail.com>
2022-08-24 22:52:22 -0300

    feat: added bootstrap to webgallery project

--- web-gallery/src/app/app.component.ts
+++ web-gallery/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Eternity';
+  url = 'https://s2.glbimg.com/EN0M2v4dElPrYaf9Ey-vFh0fDtM=/0x0:900x506/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/U/T/UhqfMrSQCiSQj4eLLXtw/genshin-impact-baal-boss-fight-900x506.jpg'
 }
