e0185da7ac0778a9f6a0dece0a5a597b3c13910b
Matheus Vieira <matheus.msv1997@gmail.com>
2022-09-11 16:41:45 -0300

    feat: resolver photo-list

--- web-gallery/src/app/app.routing.module.ts
+++ web-gallery/src/app/app.routing.module.ts
@@ -6,8 +6,11 @@ import { PhotosFormComponent } from "./photos/photos-form/photos-form.component"
 import { PhotosListComponent } from "./photos/photos-list/photos-list.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 
+// Resolvers
+import { PhotoListResolver } from "./photos/photos-list/photo-list.resolver";
+
 const routes: Routes = [
-    { path: 'user/:userName', component: PhotosListComponent},
+    { path: 'user/:userName', component: PhotosListComponent, resolve: { photos: PhotoListResolver}},
     { path: 'photo/add',   component: PhotosFormComponent},
     { path: '**',          component: NotFoundComponent}
 ];
--- web-gallery/src/app/photos/photos-list/photos-list.component.ts
+++ web-gallery/src/app/photos/photos-list/photos-list.component.ts
@@ -1,34 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+ 
 // Interfaces
 import { Photo } from '../photo';
-
-//Services
-import { PhotoService } from '../photo.service';
-
 @Component({
   selector: 'app-photos-list',
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.css']
 })
-export class PhotosListComponent implements OnInit {
+export class PhotosListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = []
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute,
-  ) {}
+  constructor( private activatedRoute: ActivatedRoute ) {}
 
   ngOnInit(): void {
-    const userName: string = this.activatedRoute.snapshot.params.userName
-    this.photoService.listFromUser(userName)
-    .subscribe(
-      ((request: Photo[]) => this.photos = request),
-      ((error) => console.log(error)),
-    )
+    this.photos = this.activatedRoute.snapshot.data.photos;
+
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
 }
